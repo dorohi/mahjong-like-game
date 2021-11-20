6e9cf3198e66ef703fde367ff50cedeb3f7c7ca2
m.m.dorohi <m.m.dorohi@gmail.com>
2021-11-20 13:21:52 +0200

    change favicon

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
 export const START_DELAY_MILLISECONDS = 5 * 1000;
-export const WRONG_DELAY_MILLISECONDS = 2 * 1000;
+export const WRONG_DELAY_MILLISECONDS = 1 * 1000;
 export const PRIME_NUMBER_START = 22;
 export const PRIME_NUMBER_END = 150;
 export const PRIME_NUMBER_MAX_COUNT = 15;
